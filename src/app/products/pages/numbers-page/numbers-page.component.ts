a82a6aa5baeaafe5456084d9d770985708c5ea81
Jose Lopez <jose.lopezberro@gmail.com>
2025-01-07 11:42:07 +0100

    introducción a la aplicación del tipo moneda y decimal

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'app-numbers-page',
   standalone: true,
-  imports: [],
+  imports: [
+    PanelModule,
+    CardModule,
+    CommonModule
+  ],
   templateUrl: './numbers-page.component.html',
   styleUrl: './numbers-page.component.css'
 })
 export class NumbersPageComponent {
+
+  public totalSells: number = 25669796.7778;
+  public percent: number = 0.6778;
+
 
 }
