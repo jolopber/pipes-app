e8dc55d7c6527b1a9a9dd83c9661be648f55e025
Jose Lopez <jose.lopezberro@gmail.com>
2025-01-07 11:13:38 +0100

    cambio de idiomas usando pipes

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { MenuModule } from 'primeng/menu';
 import { MenubarModule } from 'primeng/menubar';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
+import { PanelModule } from 'primeng/panel';
+
+
+
 
 
 @NgModule({
   exports: [
-    MenuModule,
-    MenubarModule
+    //MenuModule,
+    MenubarModule,
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
+    PanelModule
   ]
   
 })
--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { PanelModule } from 'primeng/panel';
+import { CommonModule } from '@angular/common';
+
+// Configuración idiomas
+import localeEs from '@angular/common/locales/es';
+import localeFrCA from '@angular/common/locales/fr-CA';
+
+
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData( localeEs );
+registerLocaleData( localeFrCA );
+
+
 
 @Component({
   selector: 'app-basics-page',
   standalone: true,
-  imports: [],
+  imports: [
+    PanelModule,
+    CardModule,
+    CommonModule
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' }
+  ],
   templateUrl: './basics-page.component.html',
   styleUrl: './basics-page.component.css'
 })
 export class BasicsPageComponent {
+
+  public nameLower: string = 'jose lopez';
+  public nameUpper: string = 'JOSE LOPEZ';
+  public fullName: string = 'jOsE LOpeZ berRO';
+
+  public customDate: Date = new Date();
+
+
 
 }
